e871d3909ceb786000ae687c8a495ec63f3c2da8
Grechkoseev <igorgrechkoseev@gmail.com>
2016-07-06 02:58:06 +0600

    add third state

--- src/main/java/it/sevenbits/packages/states/ChangeState.java
+++ src/main/java/it/sevenbits/packages/states/ChangeState.java
@@ -2,6 +2,7 @@ package it.sevenbits.packages.states;
 
 import it.sevenbits.packages.actions.IndentCounter;
 import it.sevenbits.packages.states.Implementations.StateOne;
+import it.sevenbits.packages.states.Implementations.StateThree;
 import it.sevenbits.packages.states.Implementations.StateTwo;
 
 /**
@@ -12,12 +13,14 @@ public class ChangeState {
     /**
      * Method that change state
      * @param symbol
+     * @param indentCounter
      * @return new state
      */
-    //это все фигня, переделывай
     public IState changeState(final Character symbol, final IndentCounter indentCounter) {
-        if(symbol.equals(';')) {
+        if (symbol.equals(';')) {
             return new StateTwo(indentCounter);
+        } else if (symbol.equals(' ')) {
+            return new StateThree(indentCounter);
         }
         return new StateOne(indentCounter);
     }
--- src/main/java/it/sevenbits/packages/states/Implementations/StateThree.java
+++ src/main/java/it/sevenbits/packages/states/Implementations/StateThree.java
@@ -3,28 +3,31 @@ package it.sevenbits.packages.states.Implementations;
 import it.sevenbits.packages.actions.IActions;
 import it.sevenbits.packages.actions.IndentCounter;
 import it.sevenbits.packages.actions.implementation.CloseBrace;
+import it.sevenbits.packages.actions.implementation.NewLine;
 import it.sevenbits.packages.actions.implementation.Nothing;
 import it.sevenbits.packages.actions.implementation.OpenBrace;
-import it.sevenbits.packages.actions.implementation.NewLine;
 import it.sevenbits.packages.states.IState;
 
 import java.util.HashMap;
 import java.util.Map;
 
 /**
- * Write code
+ * when current symbol is space
  */
-public class StateOne implements IState {
+public class StateThree implements IState {
 
     private Map<Character, IActions> hashMap;
     private IndentCounter indentCounter;
 
     /**
-     * Write code
+     * Constructor
+     *
+     * @param indentCounter
      */
-    public StateOne(final IndentCounter indentCounter) {
+    public StateThree(final IndentCounter indentCounter) {
         this.indentCounter = indentCounter;
         this.hashMap = new HashMap<Character, IActions>();
+        hashMap.put(' ', new Nothing());
         hashMap.put('{', new OpenBrace(' ', 4, indentCounter));
         hashMap.put(';', new NewLine(' '));
         hashMap.put('}', new CloseBrace(' ', 4, indentCounter));
@@ -34,7 +37,7 @@ public class StateOne implements IState {
     /**
      * write
      * @param currentSymbol
-     * @return symbol or do action
+     * @return
      */
     public String action(final Character currentSymbol) {
         if (hashMap.containsKey(currentSymbol)) {
